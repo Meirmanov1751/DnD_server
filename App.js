a43329000d012b9aacba889318edb392ba5244b6
Galizhan Tolybayev <galix.kz@gmail.com>
2022-12-08 16:32:22 +0600

    1

--- App.js
+++ App.js
@@ -3,9 +3,10 @@ const config = require('config')
 const routes = require('./routes/routes')
 const mongoose = require('mongoose')
 var bodyParser = require('body-parser')
+var cors = require('cors')
 
 const app = express()
-
+app.use(cors());
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,PATCH');
